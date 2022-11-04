75d5c0f9c9f6349cb50ec3dfcc848e058003a203
CharleeWa <18888351756@163.com>
2022-11-04 14:56:29 +0800

    feat: 使用工具类创建一个简单的组件

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 
 <template>
-  <div class="container mx-auto px-6">
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+  <div class="container mx-auto px-6 mt-20">
+    <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+      <div class="shrink-0">
+        <img class="h-12 w-12" src="./assets/vue.svg" alt="ChitChat Logo">
+      </div>
+      <div>
+        <div class="text-xl font-medium text-black">ChitChat</div>
+        <p class="text-slate-500">You have a new message!</p>
+      </div>
+    </div>
   </div>
 </template>
